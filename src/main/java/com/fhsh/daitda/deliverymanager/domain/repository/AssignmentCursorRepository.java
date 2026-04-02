ddedb4f8726275b477ba0a35fe5092d7594da74a
LAPTOP-NQ67GLI5\김다은 <tmzl235@naver.com>
2026-04-02 14:14:17 +0900

    Comment: 타입->type, 허브 아이디->hubId 주석 변경

--- src/main/java/com/fhsh/daitda/deliverymanager/domain/repository/AssignmentCursorRepository.java
+++ src/main/java/com/fhsh/daitda/deliverymanager/domain/repository/AssignmentCursorRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface AssignmentCursorRepository {
-    // 타입과 허브 아이디로 커서 조회
+    // type과 hubId로 커서 조회
     Optional<AssignmentCursor> findByTypeAndHubId(DeliveryManagerType type, UUID hubId);
 
     AssignmentCursor save(AssignmentCursor assignmentCursor);
