b5275b7943e083a8de7021dd5f93ccc0bc620500
LAPTOP-NQ67GLI5\김다은 <tmzl235@naver.com>
2026-04-03 19:55:50 +0900

    Feat: 배송담당자 생성 시 권한을 체크하도록 수정

--- src/main/java/com/fhsh/daitda/deliverymanager/presentation/controller/DeliveryManagerController.java
+++ src/main/java/com/fhsh/daitda/deliverymanager/presentation/controller/DeliveryManagerController.java
@@ -3,15 +3,16 @@ package com.fhsh.daitda.deliverymanager.presentation.controller;
 import com.fhsh.daitda.deliverymanager.application.command.CreateDeliveryManagerCommand;
 import com.fhsh.daitda.deliverymanager.application.result.CreateDeliveryManagerResult;
 import com.fhsh.daitda.deliverymanager.application.service.command.DeliveryManagerCommandService;
+import com.fhsh.daitda.deliverymanager.infrastructure.security.CustomPrincipal;
 import com.fhsh.daitda.deliverymanager.presentation.dto.request.CreateDeliveryManagerRequest;
 import com.fhsh.daitda.deliverymanager.presentation.dto.response.CreateDeliveryManagerResponse;
 import com.fhsh.daitda.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/delivery-managers")
@@ -20,8 +21,22 @@ public class DeliveryManagerController {
 
     private final DeliveryManagerCommandService service;
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'HUB')") // INTERNAL ERROR 발생
     @PostMapping
-    public ResponseEntity<CommonResponse> createDeliveryManager(@RequestBody CreateDeliveryManagerRequest request) {
+    public ResponseEntity<CommonResponse> createDeliveryManager(
+            @AuthenticationPrincipal CustomPrincipal principal,
+            @RequestBody CreateDeliveryManagerRequest request) {
+
+        if (!(principal.hasRole("ADMIN") || principal.hasRole("HUB"))) {
+            // CommonResponse에 fail 추가 후 수정 예정
+            return ResponseEntity.ok(CommonResponse.success("접근 권한이 없습니다."));
+//            return ResponseEntity
+//                    .status(HttpStatus.FORBIDDEN)
+//                    .body(CommonResponse.fail(403, "접근 권한이 없습니다."));
+        }
+
+        // Role이 HUB인 경우, 소속 hubId 확인 필요
+
         CreateDeliveryManagerCommand command = new CreateDeliveryManagerCommand(request.targetUserId(), request.type());
 
         CreateDeliveryManagerResult result = service.createDeliveryManager(command);
