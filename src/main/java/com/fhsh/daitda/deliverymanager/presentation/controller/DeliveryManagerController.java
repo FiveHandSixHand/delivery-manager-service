6494a1fede51fc95e24ed56a1134da6a4e9b3c6b
LAPTOP-NQ67GLI5\김다은 <tmzl235@naver.com>
2026-04-04 12:54:32 +0900

    Refactor: 헤더에서 role 받아오도록 변경

--- src/main/java/com/fhsh/daitda/deliverymanager/presentation/controller/DeliveryManagerController.java
+++ src/main/java/com/fhsh/daitda/deliverymanager/presentation/controller/DeliveryManagerController.java
@@ -3,15 +3,11 @@ package com.fhsh.daitda.deliverymanager.presentation.controller;
 import com.fhsh.daitda.deliverymanager.application.command.CreateDeliveryManagerCommand;
 import com.fhsh.daitda.deliverymanager.application.result.CreateDeliveryManagerResult;
 import com.fhsh.daitda.deliverymanager.application.service.command.DeliveryManagerCommandService;
-import com.fhsh.daitda.deliverymanager.infrastructure.security.CustomPrincipal;
 import com.fhsh.daitda.deliverymanager.presentation.dto.request.CreateDeliveryManagerRequest;
 import com.fhsh.daitda.deliverymanager.presentation.dto.response.CreateDeliveryManagerResponse;
 import com.fhsh.daitda.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -21,13 +17,13 @@ public class DeliveryManagerController {
 
     private final DeliveryManagerCommandService service;
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'HUB')") // INTERNAL ERROR 발생
+    //@PreAuthorize("hasAnyRole('ADMIN', 'HUB')")
     @PostMapping
     public ResponseEntity<CommonResponse> createDeliveryManager(
-            @AuthenticationPrincipal CustomPrincipal principal,
+            @RequestHeader(value = "X-User-Role", required = false) String role,
             @RequestBody CreateDeliveryManagerRequest request) {
 
-        if (!(principal.hasRole("ADMIN") || principal.hasRole("HUB"))) {
+        if (!(role.equals("ADMIN") || role.equals("HUB"))) {
             // CommonResponse에 fail 추가 후 수정 예정
             return ResponseEntity.ok(CommonResponse.success("접근 권한이 없습니다."));
 //            return ResponseEntity
